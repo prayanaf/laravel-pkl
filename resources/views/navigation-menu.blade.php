78e1798871764205343f9faeb76df39a91aca6c4
prayanaf <prayanafisya29@gmail.com>
2025-05-08 14:55:26 +0700

    6

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,18 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('pkl') }}" :active="request()->routeIs('pkl')">
+                        {{ __('PKL') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('siswa') }}" :active="request()->routeIs('siswa')">
+                        {{ __('Siswa') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('guru') }}" :active="request()->routeIs('guru')">
+                        {{ __('Guru') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('industri') }}" :active="request()->routeIs('industri')">
+                        {{ __('Industri') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\SiswaImportController;
+use App\Http\Controllers\SiswaController;
+use App\Models\siswa;
+use App\Models\guru;
+use App\Models\pkl;
+use App\Models\industri;
 
+
+Route::post('/siswa/import', [SiswaController::class, 'import'])->name('siswa.import');
 // Halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/siswa', 'siswa', ['siswa' => Siswa::all()])->name('siswa');
+Route::view('/guru', 'guru', ['guru' => Guru::all()])->name('guru');
+Route::view('/pkl', 'pkl', ['pkl' => Pkl::all()])->name('pkl');
+Route::view('/industri', 'industri', ['industri' => Industri::all()])->name('industri');
+
 
 // ========== Admin/Default Jetstream User ==========
 Route::middleware([
@@ -19,3 +31,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
